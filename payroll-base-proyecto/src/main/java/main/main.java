a268651cad125e126b02bfcc9dabcecc67ebdcb8
JeysonM <jeyson.mirabal@gmail.com>
2017-04-26 10:17:26 +0000

    dependencias para spark añadidas

--- payroll-base-proyecto/src/main/java/main/main.java
+++ payroll-base-proyecto/src/main/java/main/main.java
@@ -1,10 +1,27 @@
-package main;
+package Main;
+import static spark.Spark.*;
 
-public class main {
-
+public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		get("/", (request, response) -> hola());
+		post("/hola", (request, response) -> responder_saludo(request.queryParams("nombre_saludo")));
+		get("/Arquitectura", (request, response) -> "Hola Arquitectura");
 	}
 
+	private static String responder_saludo(String nombre) {
+		System.out.println("----------RESPONDIENDO---------");
+		return "Hola "+nombre;
+	}
+
+	private static String hola() {
+		return "<html>"
+				+ "<body>"
+				+ "<form method='post' action='/hola'>" 
+				+ "<label>Nombre:</label>"
+				+ "<input type='text' name='nombre_saludo'>"
+				+ "<input type='submit' value='Saluda'"
+				+ "</body>"
+				+ "</html>";
+		
+	}
 }
