42d774d84481e38f8019ff308e4e80df1365bbbc
JeysonM <jeyson.mirabal@gmail.com>
2017-04-28 04:35:07 +0000

    velocity agregado y funcionando

--- payroll-base-proyecto/src/main/java/main/main.java
+++ payroll-base-proyecto/src/main/java/main/main.java
@@ -2,30 +2,47 @@ package Main;
 import static spark.Spark.*;
 
 import java.lang.reflect.Array;
+import java.util.HashMap;
 
 import payrollcasestudy.boundaries.PayrollDatabase;
 import payrollcasestudy.entities.Employee;
 import payrollcasestudy.entities.views.EmployeeView;
+import spark.ModelAndView;
 import spark.Route;
+import spark.template.velocity.VelocityTemplateEngine;
 
 public class Main {
 	public static void main(String[] args) {
-		get("/", (request, response) -> root());
+		//staticFileLocation("/public");
+		get("/", (request, response) -> {
+			HashMap<String,Object> model=new HashMap<String,Object>();
+			String word="palabra";
+			model.put("word",word);
+		      return new ModelAndView(model, "indexEmployee.vtl");
+		    }, new VelocityTemplateEngine());
 		
-		//post("/hola", (request, response) -> responder_saludo(request.queryParams("nombre_saludo") ));
-		get("/employees/new", (request, response) -> EmployeeView.formCreateEmployee());
-		post("/create", (request, response) -> EmployeeView.createNewEmployee(request.queryParams("id"),request.queryParams("name"),request.queryParams("address")));
 		get("/employees", (request, response) -> EmployeeView.showAllEmployees());
+		
+		get("/employees/new", (request, response) -> {
+            return new ModelAndView(new HashMap(), "formCreateEmployee.vtl");
+        }, new VelocityTemplateEngine());
+		
+		post("/create", (request, response) -> {
+			EmployeeView.createNewEmployee(request.queryParams("id"),
+					request.queryParams("name"),request.queryParams("address"));
+            return new ModelAndView(new HashMap(), "formCreateEmployee.vtl");
+        }, new VelocityTemplateEngine());
+		
+//		get("/employees", (request, response) -> {
+//			HashMap<String,Object> model=new HashMap<String,Object>();
+//			//String word="palabra";
+//			String employees=EmployeeView.showAllEmployees();
+//			//model.put("word",word);
+//			model.put("employees",employees);
+//		      return new ModelAndView(model, "indexEmployee.vtl");
+//		    }, new VelocityTemplateEngine());
 	}
 
 	
-	private static String root() {
-		return "<html>"
-				+ "<body>"
-				+ "<h1>Pagina de Inicio</h1>" 
-				+ "</body>"
-				+ "</html>";
-		
-	}
 	
 }
--- payroll-base-proyecto/views/payrollcasestudy/entities/views/EmployeeView.java
+++ payroll-base-proyecto/views/payrollcasestudy/entities/views/EmployeeView.java
@@ -10,28 +10,11 @@ import payrollcasestudy.entities.Employee;
 
 public class EmployeeView {
 	
-	
-	public static String formCreateEmployee() {
-		return "<html>"
-				+ "<h1>Nuevo Empleado</h1>"
-				+ "<body>"
-				+ "<form method='post' action='/create'>" 
-				+ "<label>ID:</label>"
-				+ "<input type='number' name='id'>"
-				+ "<label>Nombre:</label>"
-				+ "<input type='text' name='name'>"
-				+ "<label>Direccion:</label>"
-				+ "<input type='text' name='address'>"
-				+ "<input type='submit' value='crear'>"
-				+ "</body>"
-				+ "</html>";
-	}
-	
-	public static String createNewEmployee(String employeeId, String name, String address) {
+	public static void createNewEmployee(String employeeId, String name, String address) {
 		int employeeIdInt = Integer.parseInt(employeeId);
 		Employee employee = new Employee(employeeIdInt,name,address);
         PayrollDatabase.globalPayrollDatabase.addEmployee(employeeIdInt, employee);
-        return "Exito al crear";
+        
 	}
 	
 	public static String showAllEmployees() {
