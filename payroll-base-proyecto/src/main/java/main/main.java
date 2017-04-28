5d8a22e436c34199b131c48cb8c67c29c2b55b82
JeysonM <jeyson.mirabal@gmail.com>
2017-04-28 00:03:29 +0000

    Crea un empleado con sus datos minimos

--- payroll-base-proyecto/src/main/java/main/main.java
+++ payroll-base-proyecto/src/main/java/main/main.java
@@ -1,12 +1,20 @@
 package Main;
 import static spark.Spark.*;
 
+import payrollcasestudy.boundaries.PayrollDatabase;
+import payrollcasestudy.entities.Employee;
+import spark.Route;
+
 public class Main {
 	public static void main(String[] args) {
 		get("/", (request, response) -> hola());
-		post("/hola", (request, response) -> responder_saludo(request.queryParams("nombre_saludo")));
+		post("/hola", (request, response) -> responder_saludo(request.queryParams("nombre_saludo") ));
+		get("/employee", (request, response) -> viewCreateEmployee());
+		post("/new", (request, response) -> createEmployee(request.queryParams("id"),request.queryParams("name"),request.queryParams("address")));
 		get("/Arquitectura", (request, response) -> "Hola Arquitectura");
 	}
+
+
 
 	private static String responder_saludo(String nombre) {
 		System.out.println("----------RESPONDIENDO---------");
@@ -24,4 +32,29 @@ public class Main {
 				+ "</html>";
 		
 	}
+	
+	private static String viewCreateEmployee() {
+		return "<html>"
+				+ "<h1>Nuevo Empleado</h1>"
+				+ "<body>"
+				+ "<form method='post' action='/new'>" 
+				+ "<label>ID:</label>"
+				+ "<input type='number' name='id'>"
+				+ "<label>Nombre:</label>"
+				+ "<input type='text' name='name'>"
+				+ "<label>Direccion:</label>"
+				+ "<input type='text' name='address'>"
+				+ "<input type='submit' value='crear'>"
+				+ "</body>"
+				+ "</html>";
+	}
+	
+	private static String createEmployee(String employeeId, String name, String address) {
+		int employeeIdInt = Integer.parseInt(employeeId);
+		Employee employee = new Employee(employeeIdInt,name,address);
+        PayrollDatabase.globalPayrollDatabase.addEmployee(employeeIdInt, employee);
+        return "Exito al crear";
+	}
+	
+	
 }
