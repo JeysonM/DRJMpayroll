4748706cff6e0de3cbc1863a670eef7c20498c3d
DharaRioja <cuatita_16@hotmail.com>
2017-05-28 23:31:30 -0400

    codigo hermosamente limpio, sin imports feos o comentarios...Jeyson si ensucias mi codigo te mato >.>

--- payroll-base-proyecto/src/main/java/payrollcasestudy/entities/paymentmethods/HoldMethod.java
+++
@@ -1,9 +0,0 @@
-package payrollcasestudy.entities.paymentmethods;
-
-import payrollcasestudy.entities.PayCheck;
-
-public class HoldMethod implements PaymentMethod{
-    public void pay(PayCheck payCheck) {
-        payCheck.setField("Disposition", "Hold");
-    }
-}
